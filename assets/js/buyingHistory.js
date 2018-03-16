d8cbb60a63fbc840ce76ce45ae872d962801d19d
Britz <leonardo.britz@sap.com>
2018-03-16 05:46:14 -0300

    Fucking huge and disgusting commit because I dont have time to worry about commiting every time I change something

---
+++ assets/js/buyingHistory.js
@@ -0,0 +1,96 @@
+$.getJSON("../assets/data/orderedCustomerData.json", function (data) {
+    var cupom;
+    var prevCupom;
+    var filialNome;
+    var filialEndereco;
+    var data;
+    var valorTotal = 0;
+    var ul = document.getElementById("transactions");
+
+    $.each(data, function(i, item){
+        cupom = item.Cupom;
+        if (prevCupom === undefined) {
+            valorTotal += item['Valor Venda'];
+            filialNome = item['Filial Nome'];
+            filialEndereco = item['Filial Endereco'];
+            data = item['Data Venda'];
+        } else if (cupom === prevCupom) {
+            valorTotal += item['Valor Venda'];
+        } else {
+            var li = document.createElement("li");
+            li.classList.add("timeline-inverted");
+
+            var badgeInfo = document.createElement("div");
+            badgeInfo.classList.add("timeline-badge");
+            badgeInfo.classList.add("info");
+
+            var panel = document.createElement("div");
+            panel.classList.add("timeline-panel");
+
+            var heading = document.createElement("div");
+            heading.classList.add("timeline-heading");
+
+            var title = document.createElement("h4");
+            title.classList.add("timeline-title");
+            title.appendChild(document.createTextNode(filialNome));
+
+            var body = document.createElement("div");
+            body.classList.add("timeline-body");
+
+            var address = document.createElement("h6");
+            address.classList.add("transaction-body-info");
+            address.appendChild(document.createTextNode(filialEndereco));
+
+            var transactionCost = document.createElement("h6");
+            transactionCost.classList.add("transaction-body-info");
+            transactionCost.appendChild(document.createTextNode('R$' + parseFloat(valorTotal.toFixed(2))));
+
+            var date = document.createElement("h6");
+            date.classList.add("transaction-body-info");
+            date.appendChild(document.createTextNode(data));
+
+            heading.appendChild(title);
+
+            body.appendChild(address);
+            body.appendChild(transactionCost);
+            body.appendChild(date);
+
+            panel.appendChild(heading);
+            panel.appendChild(body);
+
+            li.appendChild(badgeInfo);
+            li.appendChild(panel);
+
+            li.setAttribute("id", prevCupom);
+            ul.appendChild(li);
+
+            valorTotal = 0;
+            valorTotal += item['Valor Venda'];
+            filialNome = item['Filial Nome'];
+            filialEndereco = item['Filial Endereco'];
+            data = item['Data Venda'];
+        }
+        prevCupom = item.Cupom;
+    });
+});
+
+/**
+ * Created by singhdi on 2014-07-26.
+ */
+var app = angular.module("ionicInfiniteScrollApp",['ionic']);
+
+app.controller("InfiniteAppCntrl",function($scope){
+    $scope.moredata = false;
+    $scope.loadMoreData=function()
+    {
+        $scope.items.push({id: $scope.items.length});
+        if($scope.items.length==100)
+        {
+            $scope.moredata=true;
+        }
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    };
+
+    $scope.items=[];
+
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -456,6 +456,7 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
         });
 
 
+
         /* ---------------------------------------------- /*
          * Video popup, Gallery
          /* ---------------------------------------------- */
