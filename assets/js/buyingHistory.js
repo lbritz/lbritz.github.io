d2c6491ee50a6e9cdc1b921d3ee82869aa92ad6d
Afonso <ferrer.afonso@gmail.com>
2018-03-16 19:45:31 -0300

    .

--- assets/js/buyingHistory.js
+++ assets/js/buyingHistory.js
@@ -2,93 +2,96 @@
 var aList    = [];
 var aItems   = [];
 
-$.getJSON("../assets/data/orderedCustomerData.json", function (data) {
-    var cupom;
-    var prevCupom;
-    var filialNome;
-    var filialEndereco;
-    var data;
-    var valorTotal = 0;
-    var ul = document.getElementById("transactions");
+$(document).ready(function () {
 
-    $.each(data, function(i, item){
-        cupom = item.Cupom;
-        if (prevCupom === undefined) {
-            valorTotal += item['Valor Venda'];
-            filialNome = item['Filial Nome'];
-            filialEndereco = item['Filial Endereco'];
-            data = item['Data Venda'];
-        } else if (cupom === prevCupom) {
-            valorTotal += item['Valor Venda'];
-        } else {
-            var li = document.createElement("li");
-            li.classList.add("timeline-inverted");
+    $.getJSON("../assets/data/orderedCustomerData.json", function (data) {
+        var cupom;
+        var prevCupom;
+        var filialNome;
+        var filialEndereco;
+        var data;
+        var valorTotal = 0;
+        var ul = document.getElementById("transactions");
 
-            var badgeInfo = document.createElement("div");
-            badgeInfo.classList.add("timeline-badge");
-            badgeInfo.classList.add("info");
+        $.each(data, function(i, item){
+            cupom = item.Cupom;
+            if (prevCupom === undefined) {
+                valorTotal += item['Valor Venda'];
+                filialNome = item['Filial Nome'];
+                filialEndereco = item['Filial Endereco'];
+                data = item['Data Venda'];
+            } else if (cupom === prevCupom) {
+                valorTotal += item['Valor Venda'];
+            } else {
+                var li = document.createElement("li");
+                li.classList.add("timeline-inverted");
 
-            var panel = document.createElement("div");
-            panel.classList.add("timeline-panel");
+                var badgeInfo = document.createElement("div");
+                badgeInfo.classList.add("timeline-badge");
+                badgeInfo.classList.add("info");
 
-            var heading = document.createElement("div");
-            heading.classList.add("timeline-heading");
+                var panel = document.createElement("div");
+                panel.classList.add("timeline-panel");
+
+                var heading = document.createElement("div");
+                heading.classList.add("timeline-heading");
 
 
-            var title = document.createElement("h4");
-            title.classList.add("timeline-title");
-            title.classList.add("quicksand");
-            title.appendChild(document.createTextNode(filialNome));
+                var title = document.createElement("h4");
+                title.classList.add("timeline-title");
+                title.classList.add("quicksand");
+                title.appendChild(document.createTextNode(filialNome));
 
-            var link = document.createElement("a");
-            link.href="#";
-            link.onclick= filterItemCard();
-            link.appendChild(title);
+                var link = document.createElement("a");
+                link.href="#";
+                link.onclick= filterItemCard();
+                link.appendChild(title);
 
-            var body = document.createElement("div");
-            body.classList.add("timeline-body");
+                var body = document.createElement("div");
+                body.classList.add("timeline-body");
 
-            var address = document.createElement("h6");
-            address.classList.add("transaction-body-info");
-            address.classList.add("quicksand");
-            address.classList.add("grey-color");
-            address.appendChild(document.createTextNode(filialEndereco));
+                var address = document.createElement("h6");
+                address.classList.add("transaction-body-info");
+                address.classList.add("quicksand");
+                address.classList.add("grey-color");
+                address.appendChild(document.createTextNode(filialEndereco));
 
-            var transactionCost = document.createElement("h6");
-            transactionCost.classList.add("transaction-body-info");
-            transactionCost.classList.add("quicksand");
-            transactionCost.appendChild(document.createTextNode('R$' + parseFloat(valorTotal.toFixed(2))));
+                var transactionCost = document.createElement("h6");
+                transactionCost.classList.add("transaction-body-info");
+                transactionCost.classList.add("quicksand");
+                transactionCost.appendChild(document.createTextNode('R$' + parseFloat(valorTotal.toFixed(2))));
 
-            var date = document.createElement("h6");
-            date.classList.add("transaction-body-info");
-            title.classList.add("quicksand");
-            date.appendChild(document.createTextNode(data));
+                var date = document.createElement("h6");
+                date.classList.add("transaction-body-info");
+                title.classList.add("quicksand");
+                date.appendChild(document.createTextNode(data));
 
-            heading.appendChild(title);
+                heading.appendChild(title);
 
-            body.appendChild(address);
-            body.appendChild(transactionCost);
-            body.appendChild(date);
+                body.appendChild(address);
+                body.appendChild(transactionCost);
+                body.appendChild(date);
 
-            panel.appendChild(heading);
-            panel.appendChild(body);
+                panel.appendChild(heading);
+                panel.appendChild(body);
 
-            li.appendChild(badgeInfo);
-            li.appendChild(panel);
+                li.appendChild(badgeInfo);
+                li.appendChild(panel);
 
-            li.setAttribute("id", prevCupom);
-            ul.appendChild(li);
+                li.setAttribute("id", prevCupom);
+                ul.appendChild(li);
 
-            valorTotal = 0;
-            valorTotal += item['Valor Venda'];
-            filialNome = item['Filial Nome'];
-            filialEndereco = item['Filial Endereco'];
-            data = item['Data Venda'];
-        }
-        prevCupom = item.Cupom;
+                valorTotal = 0;
+                valorTotal += item['Valor Venda'];
+                filialNome = item['Filial Nome'];
+                filialEndereco = item['Filial Endereco'];
+                data = item['Data Venda'];
+            }
+            prevCupom = item.Cupom;
+        });
+        getShoppingData();
     });
-    getShoppingData();
-});
+}
 
 function getShoppingData(){
 
